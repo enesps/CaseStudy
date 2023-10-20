809718313221068c9885ad1e55d0116b5da7d38a
enesps <pusaenes@gmail.com>
2023-10-20 17:22:42 +0300

    feat: updated MainStorboard

--- CaseStudy/CaseStudy/ViewController.swift
+++ CaseStudy/CaseStudy/ViewController.swift
@@ -10,14 +10,21 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var nextPage: UIButton!
+    
+    @IBOutlet weak var apiKeyText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-
-    @IBAction func es(_ sender: Any) {
+        nextPage.layer.borderWidth = 1
+        nextPage.layer.cornerRadius=17
+        nextPage.layer.borderColor = UIColor.white.cgColor
+        apiKeyText.layer.borderWidth=3
+        apiKeyText.layer.borderColor = UIColor.white.cgColor
+        apiKeyText.layer.cornerRadius=10
+        
         
     }
+
     
 }
 
